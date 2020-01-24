b4765af04f3e1c2a508af5562947fc07125cad3f
Lourival Cavalcante <llcj@cesar.org.br>
2020-01-24 16:13:28 -0400

    change port value

--- src/config/upload.js
+++ src/config/upload.js
@@ -6,7 +6,7 @@ const multerS3 = require("multer-s3");
 const storageTypes = {
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: 'aircnc-storage',
+    bucket: process.env.BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
     key: (req, file, cb) => {
@@ -20,7 +20,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, '..', 'uploads'),
-  storage: storageTypes['s3'],
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
