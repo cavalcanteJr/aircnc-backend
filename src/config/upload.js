b90ab3ae31a69ad65cb50c0e59d8eeba1ad18a2a
Lourival Cavalcante <llcj@cesar.org.br>
2020-01-24 15:45:24 -0400

    including S3 services  - connection fix

--- src/config/upload.js
+++ src/config/upload.js
@@ -3,19 +3,11 @@ const path = require('path');
 const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 
-const storageTypes = {
-  local: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
-    filename: (req, file, cb) => {
-      const ext = path.extname(file.originalname);
-      const name = path.basename(file.originalname, ext);
 
-      cb(null, `${name}-${Date.now()}${ext}`);
-    },
-  }),
+module.exports = {
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: process.env.BUCKET_NAME,
+    bucket: 'aircnc-storage',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
     key: (req, file, cb) => {
@@ -25,27 +17,4 @@ const storageTypes = {
       cb(null, `${name}-${Date.now()}${ext}`);
     }
   })
-}
-
-
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: storageTypes[process.env.STORAGE_TYPE],
-  limits: {
-    fileSize: 2 * 1024 * 1024
-  },
-  fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "image/gif"
-    ];
-
-    if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Invalid file type."));
-    }
-  } 
 };
