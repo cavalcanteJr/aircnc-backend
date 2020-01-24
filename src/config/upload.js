93cc7d540d01a57c144e841c359c07ff985fd87c
Lourival Cavalcante <llcj@cesar.org.br>
2020-01-24 15:31:35 -0400

    including S3 services

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,8 +1,10 @@
 const multer = require('multer');
 const path = require('path');
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
-module.exports = {
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename: (req, file, cb) => {
       const ext = path.extname(file.originalname);
@@ -11,4 +13,39 @@ module.exports = {
       cb(null, `${name}-${Date.now()}${ext}`);
     },
   }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      const name = path.basename(file.originalname, ext);
+
+      cb(null, `${name}-${Date.now()}${ext}`);
+    }
+  })
+}
+
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes[process.env.STORAGE_TYPE],
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/gif"
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type."));
+    }
+  } 
 };
